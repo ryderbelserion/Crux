10e44cf980c416fae4092ea0c2846d82d86bb5d8
Ryder Belserion <contact@ryderbelserion.com>
2026-04-20 01:38:27 -0400

    [ci-skip] comment out cloudflare adapter for now
    This is the reason the project breaks locally.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 import starlight from "@astrojs/starlight";
+//import cloudflare from '@astrojs/cloudflare';
 
 import topics from "starlight-sidebar-topics"
 
@@ -9,6 +10,8 @@ import icon from "astro-icon";
 // https://astro.build/config
 export default defineConfig({
     site: "https://docs.ryderbelserion.com",
+
+    //adapter: cloudflare(),
 
     integrations: [starlight({
         title: "Crux",
